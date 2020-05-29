636cd197fc815fc419acbbb998b6303667494c18
Kolkos <a.van.der.kolk@kolkos.nl>
2020-05-29 14:34:53 +0200

    mail app

---
+++ chapter2.bootcustomernotification/src/test/java/nl/kolkos/chapter2/bootcustomernotification/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.kolkos.chapter2.bootcustomernotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
